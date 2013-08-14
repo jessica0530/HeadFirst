38e2253c1fb33881d04d7207f2423368c3736484
nbonnet <thebonnet09@yahoo.com>
2013-08-14 07:37:09 -0500

    HF_JQuery Ch. 3 finished

--- HF_jQuery/ch03/begin/scripts/my_scripts.js
+++ HF_jQuery/ch03/begin/scripts/my_scripts.js
@@ -16,15 +16,27 @@ $(document).ready(function() {
 	function checkForCode() {
 		var discount;
 		if($.contains(this, document.getElementById("has_discount"))) {
-			var my_num = getRandom(5);
-			discount = "<p>Your Discount is "+my_num+"%</p>";
+			var my_num = getRandom(100);
+			discount = "<p>Your Code is: CODE "+my_num+"</p>";
 		} else {
 			discount = "<p>Sorry, no discount this time!</p>";
 		}
-		$(this).append(discount);
-		//$(".guess_box").unbind();
 		$(".guess_box").each(function() {
-			$(this).unbind('click');
+			if($.contains(this, document.getElementById("has_discount"))) {
+				$(this).addClass("discount");
+			} else {
+				$(this).addClass("no_discount");
+			}
+			$(this).unbind();
 		});
+		$("#result").append(discount);
 	}
+	$(".guess_box").hover(
+		function() {
+			$(this).addClass("my_hover");
+		},
+		function() {
+			$(this).removeClass("my_hover");
+		}
+	);
 });
