0cffa4c24c37229fd4956709c3173da063c8056a
nbonnet <thebonnet09@yahoo.com>
2013-08-21 16:34:43 -0500

    HF_JQuery Ch. 8 page 309
    Added code up to this point. For some reason data from xml will not
    load. Figure this out. Test chapter end code maybe?

--- HF_jQuery/ch08/begin/scripts/my_scripts.js
+++ HF_jQuery/ch08/begin/scripts/my_scripts.js
@@ -1,7 +1,29 @@
 
 $(document).ready(function(){
-
-	getTime();
+	function getXMLRacers() {
+		$.ajax({
+			url: "finishers.xml",
+			cache: false,
+			dataType: "xml",
+			success: function(xml) { 
+				$("finishers_m").empty();
+				$("finishers_f").empty();
+				$("finishers_all").empty();
+				$(xml).find("runner").each(function() {
+					var info = '<li>Name: ' + $(this).find("fname").text() + ' ' + 
+						$(this).find("lname").text() + '. Time: ' + $(this).find("time").text() +
+						'</li>';
+					if(($this).find("gender").text() == "m") {
+						('#finishers_m').append(info);
+					} else if(($this).find("gender").text() == "f") {
+						('#finishers_f').append(info);
+					} else { }
+					('#finishers_all').append(info);
+				});
+				getTime();
+			}
+		});	
+	}
 	
 	function getTime(){
         var a_p = "";
